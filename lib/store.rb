2f1e0f46c86b6c06ceed54255c4b09e92d5c4c7b
wvv8oo <wvv8oo@gmail.com>
2016-06-05 20:54:41 +0800

    1. 根目录必需挂载；2. 替换items为children; 3. 修改排序

--- lib/store.rb
+++ lib/store.rb
@@ -5,7 +5,11 @@ require_relative './util'
 
 class Store
     def initialize(files)
-        @items_key = '__items__'
+        #直接子级的key
+        @children_key = '__children__'
+        #所有后代的key
+        @posterity_key = '__posterity__'
+        #所有文章
         @article = Article.new
 
         @data = {
@@ -13,7 +17,7 @@ class Store
             'articles' => self.get_articles(files),
             #目录树
             'tree' => {
-                @items_key => Array.new
+                @children_key => Array.new
             }
         }
 
@@ -30,12 +34,12 @@ class Store
     end
 
     #从节点数据中, 获取items
-    def get_items(node)
-        node[@items_key]
+    def get_children(node)
+        node[@children_key]
     end
 
-    def is_items_key(key)
-        key == @items_key
+    def is_children_key(key)
+        key == @children_key
     end
 
     #获取所有的文章列表
@@ -58,44 +62,47 @@ class Store
         @data['articles'].each{ |key, article|
             dir = File::dirname(article['relative_path'])
             relative_path_md5 = article['relative_path_md5']
-
-            if dir == '.'
-                #挂至根目录的
-                @data['tree'][@items_key].push relative_path_md5
-            else
-                # 将文件挂至节点
-                this.mount_node_to_tree dir, relative_path_md5
-            end
+            this.mount_node_to_tree dir, relative_path_md5
         }
     end
 
-    #挂到节点上, 如果不在
+    #挂到节点上, 如果不在则创建
     def mount_node_to_tree(path, relative_path_md5)
         node = @data['tree']
+
+        if path == '.'
+            node[@children_key].push relative_path_md5
+            return
+        end
+
         path.split('/').each{ |segment|
             current_node = node[segment]
             if not current_node
                 current_node = Hash.new()
-                current_node[@items_key] = Array.new
+                current_node[@children_key] = Array.new
                 node[segment] = current_node
             end
 
             node = current_node
-            node[@items_key].push relative_path_md5
+            node[@children_key].push relative_path_md5
+
+            #所有的子级，都要向root插入数据
+            @data['tree'][@children_key].push relative_path_md5
         }
     end
+
 
     #给所有的文件夹排序
     def sort(node)
         this = self
         node.each { | key, current |
             #items, 需要进行排序
-            if key == @items_key
+            if key == @children_key
                 #根据文章的最后修改日期进行排序
                 current.sort! {|left, right|
                     left_article = @data['articles'][left]
                     right_article = @data['articles'][right]
-                    left_article['mtime'] <=> right_article['mtime']
+                    right_article['mtime'] <=> left_article['mtime']
                 }
             else
                 #递归调用进行排序
