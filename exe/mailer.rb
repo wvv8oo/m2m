7ad9f8df64ce18e2371c3d886db1ec67e1f557be
wvv8oo <wvv8oo@gmail.com>
2016-06-09 19:39:55 +0800

    增加强制发送的参数，修正t参数的冲突问题

--- exe/mailer.rb
+++ exe/mailer.rb
@@ -7,16 +7,17 @@ module MailCommand
 		c.summary = '将Markdown转换为HTML，并邮件发送给某人'
 		c.description = ''
 		c.option '-s STRING', '--subject STRING', String, '邮件主题，可选'
-		c.option '-t STRING', '--to STRING', String, '要发送的人，可选'
-		c.option '-f STRING', '--file STRING', String, '要发送的markdown文件，如果不指定，则使用最近修改的文件'
+		c.option '-a STRING', '--addressee STRING', String, '收件人，可选'
+		c.option '--force', '强行发送，不提示'
+		c.option '-m STRING', '--markdown STRING', String, '要发送的markdown文件，如果不指定，则使用最近修改的文件'
 		c.action do |args, options|
 		
 		util = Util.instance
 		#合并目录
 		util.workbench = util.get_merge_path('./')
-		
+
 		mailer = Mailer.new
-		mailer.execute options.file, options.to, options.subject
+		mailer.send options.addressee, options.markdown, options.subject, options.force
 		end
 	end
 end
