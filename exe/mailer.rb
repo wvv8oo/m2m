8b59204094a651f6310e9474f13ca5e669938f5c
wvv8oo <wvv8oo@gmail.com>
2016-06-09 22:48:46 +0800

    更改force为slient

--- exe/mailer.rb
+++ exe/mailer.rb
@@ -3,12 +3,12 @@ require_relative '../lib/mailer'
 
 module MailCommand
 	def mail(c)
-		c.syntax = 'm2b mail [options]'
+		c.syntax = 'm2m mail [options]'
 		c.summary = '将Markdown转换为HTML，并邮件发送给某人'
 		c.description = ''
 		c.option '-s STRING', '--subject STRING', String, '邮件主题，可选'
 		c.option '-a STRING', '--addressee STRING', String, '收件人，可选'
-		c.option '--force', '强行发送，不提示'
+		c.option '--slient', '静默发送，不提示'
 		c.option '-m STRING', '--markdown STRING', String, '要发送的markdown文件，如果不指定，则使用最近修改的文件'
 		c.action do |args, options|
 		
@@ -17,7 +17,7 @@ module MailCommand
 		util.workbench = util.get_merge_path('./')
 
 		mailer = Mailer.new
-		mailer.send options.addressee, options.markdown, options.subject, options.force
+		mailer.send options.addressee, options.markdown, options.subject, options.slient
 		end
 	end
 end
