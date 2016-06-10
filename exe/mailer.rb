e09700e4883c54ff40abab490f814422401f8ea0
wvv8oo <wvv8oo@gmail.com>
2016-06-10 17:18:16 +0800

    增加邮件和密码配置的入口

--- exe/mailer.rb
+++ exe/mailer.rb
@@ -1,5 +1,6 @@
 require 'commander'
 require_relative '../lib/mailer'
+require_relative '../lib/setup'
 
 module MailCommand
 	def mail(c)
@@ -9,15 +10,24 @@ module MailCommand
 		c.option '-s STRING', '--subject STRING', String, '邮件主题，可选'
 		c.option '-a STRING', '--addressee STRING', String, '收件人，可选'
 		c.option '--slient', '静默发送，不提示'
+		c.option '--setup', '配置邮件的相关信息'
+		c.option '--password', '配置邮件的密码'
 		c.option '-m STRING', '--markdown STRING', String, '要发送的markdown文件，如果不指定，则使用最近修改的文件'
 		c.action do |args, options|
-		
-		util = Util.instance
-		#合并目录
-		util.workbench = util.get_merge_path('./')
+			util = Util.instance
+			setup = Setup.instance
 
-		mailer = Mailer.new
-		mailer.send options.addressee, options.markdown, options.subject, options.slient
+			#合并目录
+			util.workbench = util.get_merge_path('./')
+
+			if options.setup
+				setup.ask_mail
+			elsif options.password
+				setup.ask_mail_password
+			else
+				mailer = Mailer.new
+				mailer.send options.addressee, options.markdown, options.subject, options.slient
+			end
 		end
 	end
 end
