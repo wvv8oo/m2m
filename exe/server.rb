2afbf92273c654f0e24472c5be6de841b36ced2c
wvv8oo <wvv8oo@gmail.com>
2016-06-11 20:39:38 +0800

    支持指定端口

--- exe/server.rb
+++ exe/server.rb
@@ -8,15 +8,14 @@ module ServerCommand
 		c.syntax = 'm2m server [options]'
 		c.summary = ''
 		c.description = '创建一个服务器'
-		# c.option '--watch', '监控文件的变化'
+		c.option '-p INTEGER', '--port INTEGER', Integer, '生成的目标目录'
 		c.action do |args, options|
 			util = Util.instance
 			setup = Setup.instance
 
 			#合并目录
 			util.workbench = util.get_merge_path './'
-
-			Server.new.start
+			Server.new.start options.port
 		end
 	end
 end
