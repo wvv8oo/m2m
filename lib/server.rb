f4f384265782c216dd16f9a9e7f4e1ae47d2a944
wvv8oo <wvv8oo@gmail.com>
2016-06-11 20:40:06 +0800

    改用webrick

--- lib/server.rb
+++ lib/server.rb
@@ -1,9 +1,18 @@
-require 'rack'
+require 'webrick'
 require_relative './util'
 require_relative './setup'
 
-class Server < ::Rack::Server
-	def app
-		Rack::Directory::new Setup.instance.target_dir
+# class Server < ::Rack::Server
+# 	def app
+# 		Rack::Directory::new Setup.instance.target_dir
+# 	end
+# end
+
+class Server
+	def start(port)
+		port = port || 8000
+		root = Setup.instance.target_dir
+		server = WEBrick::HTTPServer.new :Port => port, :DocumentRoot => root
+		server.start
 	end
 end
